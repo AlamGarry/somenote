e293451696e5420f2b89f4c07b28aa709353d7b2
AlamGarry <alqifariq@gmail.com>
2024-06-13 14:45:42 +0700

    add task input

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,21 +1,47 @@
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TextInput,
+  Button,
+} from 'react-native';
 import TaskListItem from './TaskListItem';
 import { useState } from 'react';
 
 export default function TaskList() {
-  const [task, setTask] = useState([
+  const [tasks, setTasks] = useState([
     { description: 'First Task' },
     { description: 'Second Task' },
   ]);
+
+  const [newTask, setNewTask] = useState('');
+
+  const createTask = () => {
+    console.log('Create Task', newTask);
+    setTasks([...tasks, { description: newTask }]);
+    setNewTask('');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>TaskList</Text>
       {/* Task List Items */}
       <FlatList
-        data={task}
+        data={tasks}
         contentContainerStyle={{ gap: 5 }}
         renderItem={({ item }) => <TaskListItem task={item} />}
       />
+
+      {/* New task input */}
+      <TextInput
+        value={newTask}
+        onChangeText={setNewTask}
+        placeholder='New task'
+        placeholderTextColor='gray'
+        style={styles.input}
+      />
+      <Button title='Add Task' onPress={() => createTask()} />
     </View>
   );
 }
@@ -25,10 +51,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#101112',
     padding: 10,
     borderRadius: 5,
+    gap: 5,
   },
   text: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 16,
+  },
+  input: {
+    color: 'white',
+    padding: 15,
+    backgroundColor: '#1D2125',
+    borderRadius: 5,
   },
 });
