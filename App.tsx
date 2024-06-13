8a8c4cb8ada0000bfd1a20b6f77fddfdd46f85f3
AlamGarry <alqifariq@gmail.com>
2024-06-13 21:07:33 +0700

    linear gradient

--- App.tsx
+++ App.tsx
@@ -2,15 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
 import TaskListItem from './src/components/TaskListItem';
 import TaskList from './src/components/TaskList';
+import TaskBoard from './src/components/TaskBoard';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ padding: 10 }}>
-        <TaskList />
-      </View>
-      <StatusBar style='auto' />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <TaskBoard />
+      <StatusBar style='light' />
+    </View>
   );
 }
 
